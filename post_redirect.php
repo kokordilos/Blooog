9f5051e0fc90f5e68ea4fbf4c6a120e1b7df64b5
Harut <harutghandilyan@gmail.com>
2017-12-21 00:54:18 +0400

    css

--- post_redirect.php
+++ post_redirect.php
@@ -5,10 +5,18 @@ include '/connect_prop.php';
 date_default_timezone_set('Europe/Moscow');
 $date = date('Y-m-d H:i:s');
 
-if(!empty($_POST["new_comment"])){    
+
+if(!empty($_POST["new_comment"])){ 
+    $comment =   $_POST["new_comment"] ;
     $post = "INSERT INTO `comment` (`id`, `user_id`, `comment`, `date`) 
-    VALUES (NULL,'" . $_SESSION["id"] . "', '" . $_POST["new_comment"] . "', '" . $date . "')";    
-    $conn->exec($post);
+    VALUES (NULL,'" . $_SESSION["id"] . "', :comment , '" . $date . "')";
+
+    var_dump($comment);    
+    var_dump(htmlentities($post));
+    $post= $conn->prepare($post);
+    $post->bindParam(':comment', $comment);
+    $post->execute();
+    
     header("LOCATION: /php_tut/?page=profile");
     
 }
